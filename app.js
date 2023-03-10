6c580c8ff97a2a34c5ad7a3712e7b0d3a07d47a7
Vito Senič <vito.senic@gmail.com>
2023-03-10 10:47:03 +0100

    adding html

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get("/", function(req, res) {
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Maribor&units=metric&lang=sl&appid=fe3f4adfab786bae3c4dac4635567807";
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");    
+});
+
+app.post("/", function(req, res) {
+    console.log(req.body.cityName);
+
+    const query = req.body.cityName;
+    const apiKey = "fe3f4adfab786bae3c4dac4635567807";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query +"&units=" + unit + "&lang=sl&appid=" + apiKey;
 
     https.get(url, function(response){
         console.log('statusCode:', response.statusCode);
-
+        
         response.on("data", function(data) {
             //That's command put weather data into json format
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const feels_like = weatherData.main.feels_like;
             const weather_description = weatherData.weather[0].description;
-
+            
             const icon = weatherData.weather[0].icon;
             const icon_url = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
             
             res.setHeader('Content-Type', 'text/html; charset=utf-8');
-            res.write("<h1>The temperature in Maribor is " + temp + " degrees Celcius </h1>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius </h1>");
             res.write("<h1>Tukaj je " + weather_description + "</h1>");
             res.write("<img src=" + icon_url + ">");
             res.send();
